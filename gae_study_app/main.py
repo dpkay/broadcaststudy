9fa5a57a0635176a15f5bfe047f7d3b5e66761da
dpk <dpk@rz-public-dock-230-dhcp.ethz.ch>
2012-07-13 16:44:32 +0200

    test

--- gae_study_app/main.py
+++ gae_study_app/main.py
@@ -6,6 +6,7 @@ import jinja2
 import os
 import yaml
 import json
+import sys
 
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -18,12 +19,24 @@ jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
+class SegmentDict(dict):
+  def __init__(self, filename):
+    with file(filename, 'r') as stream:
+      super(SegmentDict, self).__init__(yaml.load(stream))
+#      for key, clips in yaml.load(stream).iteritems()
+#        self[key] = item
+
+#distinguishedSegments = SegmentDict('distinguished_segments.yaml')
+
 class TaskDict(dict):
   def __init__(self):
     with file('tasks.yaml', 'r') as stream:
       for item in yaml.load(stream):
         self[item['name']] = item
 
+
+print "foo"
+print TaskDict()
 
 class Vote(db.Model):
   voteKey = db.StringProperty()
@@ -55,12 +68,27 @@ class MainPage(webapp2.RequestHandler):
           self.redirect(users.create_login_url(self.request.uri))
           return
 
+        # initialize segments
+        distinguishedSegments = SegmentDict('distinguished_segments.yaml')
+
+        taskdict = {}
+        for key, data in distinguishedSegments.iteritems():
+          prettyName = 'D' + str(key)
+          name = 'segment%03d' % key
+          task = {}
+          task['name'] = name
+          task['prettyName'] = prettyName
+          task['youtube_id1'] = data[1]
+          task['youtube_id2'] = data[7]
+          task['youtube_id3'] = data[8]
+          taskdict[name] = task
+
         # initialize task
-        taskdict = TaskDict()
+        #taskdict = TaskDict()
 
         # read votes from db and augment task dictionary
         votesQuery = Vote.all().ancestor(userKey())
-        votes = votesQuery.fetch(100)
+        votes = votesQuery.fetch(500)
         for vote in votes:
           taskName, suffix = vote.voteKey.split('_') 
           taskdict[taskName]['selected_%s' % suffix] = vote.value
@@ -85,22 +113,47 @@ class RPCHandler(webapp2.RequestHandler):
     action = req['action']
     if action == "vote":
       value = int(req['value'])
-      if value > 0: 
-        result = {"status": "stored"}
-      else:
-        result = {"status": 0}
+      result = {'status': 'undefined'}
 
-      voteQuery = Vote.all().ancestor(userKey()).filter("voteKey = ", req['key'])
+      # test whether opposite has same value. if yes then fail
+      key = req['key']
+      keySplit = req['key'].split('_')
+      keyOpposite = keySplit[0] + '_' + ('best' if keySplit[1] == 'worst' else 'worst')
+      voteQuery = Vote.all().ancestor(userKey()).filter("voteKey = ", keyOpposite)
       votes = voteQuery.fetch(1)
-      try:
-        vote = votes[0]
-      except IndexError:
-        vote = Vote(parent=userKey())
-        vote.voteKey = req['key']
-      vote.value = value
-      vote.put()
+      if value>0 and len(votes)>0 and int(votes[0].value) == int(req['value']):
+        # don't allow best and worst to have the same value
+        result['status'] = "failed"
 
+        # jump back to old value
+        voteQuery = Vote.all().ancestor(userKey()).filter("voteKey = ", key)
+        votes = voteQuery.fetch(1)
+        try:
+          oldValue = votes[0].value
+        except IndexError:
+          oldValue = 0
+        result['oldValue'] = oldValue
+
+      else: 
+        # ok, that value is different from the opposite... store it to db
+        voteQuery = Vote.all().ancestor(userKey()).filter("voteKey = ", key)
+        print >>sys.stderr, keyOpposite
+        votes = voteQuery.fetch(1)
+        try:
+          vote = votes[0]
+        except IndexError:
+          vote = Vote(parent=userKey())
+          vote.voteKey = req['key']
+        vote.value = value
+        vote.put()
+        if value>0:
+          result['status'] = "stored"
+        else:
+          result['status'] = "neutral"
+
+      # send result to client
       self.response.out.write(json.dumps(result))
+
     else:
       #self.response.out.write(json.loads(self.request.body))
       self.error(403)
--- gae_study_app/voting.js
+++ gae_study_app/voting.js
@@ -1,4 +1,4 @@
-function foo(button) {
+function doVote(button) {
   $.ajax({
     url: 'rpc',
     data: JSON.stringify({
@@ -15,14 +15,36 @@ function foo(button) {
       var status = result["status"];
       var tableRow = button.parent().parent();
       var questionRowName = 'question_' + button.attr('name');
-      //alert(questionRowName);
       var questionRow = $('#' + questionRowName);
+      
+      var splitName = button.attr('name').split('_'); 
+      var prefix = splitName[0];
+      var suffix = splitName[1];
+      //var oppositeSuffix = suffix=="best" ? "worst" : "best";
+      //var opposite = $('#'+prefix+'_'+oppositeSuffix+'_'+button.attr('value'));
       if (status == "stored") {
         tableRow.addClass('completed');
         questionRow.addClass('completed');
       } else {
         tableRow.removeClass('completed');
         questionRow.removeClass('completed');
+      }
+      if (status == "failed") {
+        oldId = button.attr('name')+'_'+result['oldValue'];
+        $('#' + oldId).attr("checked", "checked");
+        tableRow.addClass('failed');
+        questionRow.addClass('failed');
+        setTimeout(function() {
+          tableRow.removeClass('failed');
+          questionRow.removeClass('failed');
+          if(result['oldValue']>0) {
+            tableRow.addClass('completed');
+            questionRow.addClass('completed');
+          }
+        }, 250);
+      } else {
+        tableRow.removeClass('failed');
+        questionRow.removeClass('failed');
       }
       updateCount();
     }
@@ -37,5 +59,32 @@ function updateCount() {
   $('#completedcount').text(num_completed);
   $('#totalcount').text(num_total);
   $('#percentage').text(percentage+"%");
-  //alert(num_completed);
 }
+
+function playAll(ids) {
+  window.cuedIds = [];
+  for (var i=0; i<ids.length; i++) {
+    var id = ids[i];
+    var containerName = ids[i] + "_container";
+    var player = YoutubePlayer.findById(containerName);
+    if(!player) {
+      var player = new YoutubePlayer(containerName, ids[i], {
+        width: 320,
+        height: 205 });
+
+      player.on('cued', function(eventName, videoId) {
+        window.cuedIds.push(videoId);
+        if(window.cuedIds.length == 3) {
+          for (var i=0; i<3; i++) {
+            var player = document.getElementById(cuedIds[i] + "_container");
+            player.playVideo();
+          }
+        }
+      });
+    } else {
+      player.ref.seekTo(0);
+      player.ref.playVideo();
+    } 
+  }
+}
+
