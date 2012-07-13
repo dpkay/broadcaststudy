d49b6b92be9b56dbabdc5aee737ec61cbe060ed3
Dominik Kaeser <dominik.kaeser@gmail.com>
2012-07-13 18:09:17 +0200

    randomness done

--- gae_study_app/main.py
+++ gae_study_app/main.py
@@ -7,6 +7,8 @@ import os
 import yaml
 import json
 import sys
+import random
+import operator
 
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -28,19 +30,20 @@ class SegmentDict(dict):
 
 #distinguishedSegments = SegmentDict('distinguished_segments.yaml')
 
-class TaskDict(dict):
-  def __init__(self):
-    with file('tasks.yaml', 'r') as stream:
-      for item in yaml.load(stream):
-        self[item['name']] = item
-
-
-print "foo"
-print TaskDict()
+#class TaskDict(dict):
+#  def __init__(self):
+#    with file('tasks.yaml', 'r') as stream:
+#      for item in yaml.load(stream):
+#        self[item['name']] = item
+#
+#
+#print "foo"
+#print TaskDict()
 
 class Vote(db.Model):
   voteKey = db.StringProperty()
-  value = db.IntegerProperty()
+  cameraName = db.StringProperty()
+  #value = db.IntegerProperty()
   #date = db.DateTimeProperty(auto_now_add=True)
 
 
@@ -58,7 +61,7 @@ def userKey(user_id=userId()):
 def userName():
   return users.get_current_user().nickname()
 
-
+random.seed(userName())
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -69,19 +72,42 @@ class MainPage(webapp2.RequestHandler):
           return
 
         # initialize segments
-        distinguishedSegments = SegmentDict('distinguished_segments.yaml')
+        #distinguishedSegments = SegmentDict('distinguished_segments.yaml')
+        segments = SegmentDict('segments.yaml')
+        distinguishedIndexes = [40, 80, 120, 160, 200,
+                                240, 280, 320, 360, 400]
+
+        normalIndexes = set(segments.keys()) - set(distinguishedIndexes)
+        #distinguishedSegments = {}
+        #for i in distinguishedIndexes:
+        #  distinguishedSegments[i] = allSegments[i]
+
+        chosenIndexes = random.sample(distinguishedIndexes, 5) +\
+                        random.sample(normalIndexes, 10)
 
         taskdict = {}
-        for key, data in distinguishedSegments.iteritems():
-          prettyName = 'D' + str(key)
-          name = 'segment%03d' % key
+        #for key, data in distinguishedSegments.iteritems():
+        for index in chosenIndexes:
+          data = segments[index]
+           
+          prettyPrefix = 'D' if index in distinguishedIndexes else 'N'
+          #name = 'segment%03d' % index 
           task = {}
+          task['prettyName'] = prettyPrefix + str(index)
+          name = task['prettyName']
           task['name'] = name
-          task['prettyName'] = prettyName
-          task['youtube_id1'] = data[1]
-          task['youtube_id2'] = data[7]
-          task['youtube_id3'] = data[8]
+          task['index'] = index
+
+          shuffledIds = ['A','B','C']
+          random.shuffle(shuffledIds)
+          task['youtube_id1'] = data[shuffledIds[0]]
+          task['youtube_id2'] = data[shuffledIds[1]]
+          task['youtube_id3'] = data[shuffledIds[2]]
+          task['camera1'] = shuffledIds[0]
+          task['camera2'] = shuffledIds[1]
+          task['camera3'] = shuffledIds[2]
           taskdict[name] = task
+
 
         # initialize task
         #taskdict = TaskDict()
@@ -91,18 +117,20 @@ class MainPage(webapp2.RequestHandler):
         votes = votesQuery.fetch(500)
         for vote in votes:
           taskName, suffix = vote.voteKey.split('_') 
-          taskdict[taskName]['selected_%s' % suffix] = vote.value
+          taskdict[taskName]['selected_%s' % suffix] = vote.cameraName
 
         # we can now throw away the keys, so
         # convert augmented task dictionary to list.
+        #tasks = sorted(taskdict.iteritems(), key=operator.itemgetter(1))
+        #tasks = sorted(taskdict.itervalues(), key=lambda x: x['index'])
         tasks = taskdict.values()
+        random.shuffle(tasks)
 
         template_values = {
             'username': userName(),
             'logout_url': logout_url,
             'tasks': tasks
         }
-
         template = jinja_environment.get_template('voting_template.html')
         self.response.out.write(template.render(template_values))
 
@@ -112,7 +140,9 @@ class RPCHandler(webapp2.RequestHandler):
     req = json.loads(self.request.body)
     action = req['action']
     if action == "vote":
-      value = int(req['value'])
+      valueSplit = req['value'].split('_')
+      radioValue = valueSplit[0]
+      cameraName = valueSplit[1]
       result = {'status': 'undefined'}
 
       # test whether opposite has same value. if yes then fail
@@ -121,18 +151,20 @@ class RPCHandler(webapp2.RequestHandler):
       keyOpposite = keySplit[0] + '_' + ('best' if keySplit[1] == 'worst' else 'worst')
       voteQuery = Vote.all().ancestor(userKey()).filter("voteKey = ", keyOpposite)
       votes = voteQuery.fetch(1)
-      if value>0 and len(votes)>0 and int(votes[0].value) == int(req['value']):
+      if radioValue>0 and len(votes)>0 and votes[0].cameraName == cameraName:
         # don't allow best and worst to have the same value
         result['status'] = "failed"
 
         # jump back to old value
         voteQuery = Vote.all().ancestor(userKey()).filter("voteKey = ", key)
         votes = voteQuery.fetch(1)
-        try:
-          oldValue = votes[0].value
-        except IndexError:
-          oldValue = 0
-        result['oldValue'] = oldValue
+#####        try:
+#####          oldValue = votes[0].value
+#####        except IndexError:
+#####          oldValue = 0
+#####        result['oldValue'] = oldValue
+#####   JUST JUMP BACK TO 0, THATS OK
+        result['oldValue'] = 0
 
       else: 
         # ok, that value is different from the opposite... store it to db
@@ -144,9 +176,9 @@ class RPCHandler(webapp2.RequestHandler):
         except IndexError:
           vote = Vote(parent=userKey())
           vote.voteKey = req['key']
-        vote.value = value
+        vote.cameraName = cameraName
         vote.put()
-        if value>0:
+        if cameraName != '0':
           result['status'] = "stored"
         else:
           result['status'] = "neutral"
